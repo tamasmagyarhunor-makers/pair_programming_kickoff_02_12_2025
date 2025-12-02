97b709ca2289a7620765b3b22ea14ff7a06d6086
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2025-12-02 10:50:38 +0000

    Add code to raise TypeError when not int passed in

--- lib/fizzbuzz.py
+++ lib/fizzbuzz.py
@@ -1,4 +1,6 @@
 def fizzbuzz(number):
+    if type(number) != int:
+        raise TypeError("Input can only be integer!")
     if number % 15 == 0:
         return 'fizzbuzz'
     if number % 3 == 0:
